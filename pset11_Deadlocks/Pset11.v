eed232fa597eb80026633c182e8bf0fe997886d6
McCoy Becker <mccoybecker@gmail.com>
2022-05-04 14:35:03 -0400

    init.

--- pset11_Deadlocks/Pset11.v
+++ pset11_Deadlocks/Pset11.v
@@ -34,11 +34,139 @@ Example bad: prog. Admitted.
 (* The two questions above are not graded, but we hope they help you understand
    the material better! *)
 
+Lemma who_has_the_lock'' : forall h a l l1 c l2,
+      goodCitizen l1 c l2
+      -> a \in l1
+      -> l1 \subseteq l
+      -> finished c
+         \/ (exists h' l' c', step0 (h, l, c) (h', l', c'))
+         \/ (exists a', a' < a /\ a' \in l).
+Proof.
+  induct 1.
+  eauto.
+  intros.
+  admit.
+  intros.
+  right.
+  left.
+  eauto.
+  intros.
+  right.
+  left.
+  eauto.
+  intros.
+  right.
+  assert (~a0 \in l0).
+  eapply notin_from_order.
+  equality.
+  eapply H in H0.
+  admit.
+  intros.
+  right.
+  left.
+  assert (a0 \in l).
+  sets.
+  eapply StepUnlock in H2.
+  eauto.
+Admitted.
+
+Lemma who_has_the_lock' : forall h a l l1 c,
+      goodCitizen l1 c {}
+      -> a \in l1
+      -> l1 \subseteq l
+      -> (exists h' l' c', step0 (h, l, c) (h', l', c'))
+        \/ (exists a', a' < a /\ a' \in l).
+Proof.
+  induct 1. intros; try sets.
+  intros.
+  eapply GcBind in H0.
+  eapply who_has_the_lock'' in H0.
+  first_order.
+  invert H0.
+  left.
+  eauto.
+  right.
+  eauto.
+  eauto.
+  eauto.
+  eauto.
+  eauto.
+  eauto.
+  intros.
+  sets.
+  intros.
+  assert (a0 \in l).
+  sets.
+  left.
+  eapply StepUnlock in H.
+  eauto.
+  Unshelve.
+  eauto.
+Qed.
+
+Lemma who_has_the_lock : forall l h a p,
+      Forall (fun l_c => goodCitizen (fst l_c) (snd l_c) {}) p
+      -> a \in locksOf p
+      -> locksOf p \subseteq l
+      -> (exists h_l_p', step (h, l, progOf p) h_l_p')
+        \/ (exists a', a' < a /\ a' \in l).
+Proof.
+  induct 1; intros; try sets.
+  eapply who_has_the_lock' in H.
+  first_order.
+  apply StepThread with (cs1 := []) (cs2 := progOf l0) in H.
+  left.
+  simplify.
+  admit.
+  right.
+  eauto.
+  admit.
+Admitted.
+
+Theorem if_lock_held_then_progress : forall bound a h p,
+    Forall (fun l_c => goodCitizen (fst l_c) (snd l_c) {}) p
+    -> a \in locksOf p
+    -> a < bound
+    -> Forall (fun l_c => finished (snd l_c)) p
+       \/ exists h_l_p', step (h, locksOf p, progOf p) h_l_p'.
+Proof.
+  intros.
+  invert H.
+  sets.
+  apply who_has_the_lock with
+    (l := locksOf (x :: l))
+    (h := h) in H0.
+  first_order.
+  admit.
+  eauto.
+  eauto.
+Admitted.
+
+Theorem if_no_locks_held_then_progress : forall h p,
+      Forall (fun l_c => goodCitizen (fst l_c) (snd l_c) {}) p
+      -> locksOf p = {}
+      -> Forall (fun l_c => finished (snd l_c)) p
+        \/ exists h_l_p', step (h, locksOf p, progOf p) h_l_p'.
+Proof.
+  induct 1; intros; try sets.
+  cases x; simplify.
+  assert (locksOf l = {}).
+  sets.
+  assert (s = {}).
+  sets.
+  rewrite H3.
+  first_order.
+  admit.
+  eapply step_cat in H4.
+  right.
+  replace ({  } \cup ({  } \cup locksOf l)) with (locksOf l) by sets.
+  eauto.
+Admitted.
+
 (* Since we use the [a_useful_invariant] theorem, proving [deadlock_freedom]
    reduces to proving this lemma — the only one in this Pset!  See hints at the bottom
-   of the signature file if you get stuck, and of course come to office hours if you 
+   of the signature file if you get stuck, and of course come to office hours if you
    have any questions or need help. *)
-
 
 Module Impl.
 (* HINT 1-5 (see Pset11Sig.v) *)   
@@ -48,7 +176,37 @@ Lemma deadlock_freedom' :
   Forall finished (progOf p) \/ (exists h_l_p' : heap * locks * prog,
                                     step (h, locksOf p, progOf p) h_l_p').
 Proof.
-Admitted.
+  intros.
+  excluded_middle (exists a, a \in locksOf p); simplify.
+  invert H0.
+  eapply if_lock_held_then_progress in H.
+  destruct H.
+  assert (Forall finished (progOf p)).
+  eapply finished_progOf.
+  equality.
+  equality.
+  assert (exists h_l_p' : heap * locks * prog, step (h, locksOf p, progOf p) h_l_p').
+  eapply H.
+  equality.
+  apply H1.
+  assert (x < x + 1).
+  linear_arithmetic.
+  apply H0.
+  unfold not in H0.
+  eapply if_no_locks_held_then_progress in H.
+  destruct H.
+  assert (Forall finished (progOf p)).
+  eapply finished_progOf.
+  equality.
+  equality.
+  assert (exists h_l_p' : heap * locks * prog, step (h, locksOf p, progOf p) h_l_p').
+  eapply H.
+  equality.
+  remember (locksOf p).
+  sets.
+  eapply H0.
+  first_order.
+Qed.
 
 (* Here's how we can use [a_useful_invariant] to go from [deadlock_freedom'] to
    [deadlock_freedom]: *)
--- pset12_RelyGuarantee/Pset12.v
+++ pset12_RelyGuarantee/Pset12.v
@@ -3,6 +3,12 @@
 Require Import Frap Pset12Sig.
 
 Module Impl.
+
+Ltac existT_subst :=
+   repeat match goal with
+   | [ H : existT _ _ _ = existT _ _ _ |- _ ] => eapply inj_pair2 in H
+     end; subst.
+
 (* Part 1: read Pset12Sig.v and answer the questions below. This task is
  * ungraded, but we are assigning it in hope it helps you complete the
  * following parts.
@@ -60,7 +66,7 @@ Module Impl.
 - What syntactic forms of commands does [atomic] handle?
 
 - What is the argument to the tactic [atomic]?
-*)
+ *)
 
 (* Part 2: prove a program *)
 
@@ -75,12 +81,48 @@ Lemma ht_increment : forall init,
     (fun h => h $! 0 = init)
     (fun _ _ => False)
     (   (tmp <- Atomic (Read 0); Atomic (Write 0 (tmp + 1)))
-     || (tmp <- Atomic (Read 0); Atomic (Write 0 (tmp + 1)))
+        || (tmp <- Atomic (Read 0); Atomic (Write 0 (tmp + 1)))
     )
     (fun _ _ => True)
     (fun _ h => h $! 0 > init).
 Proof.
-Admitted.
+  simp.
+  fork (fun h => h $! 0 >= init)
+    (fun h h' => h = h' \/ h' $! 0 > init)
+    (fun (_ : unit) h => h $! 0 > init)
+    (fun h => h $! 0 >= init)
+    (fun h h' => h = h' \/ h' $! 0 > init)
+    (fun (_ : unit) h => h $! 0 > init).
+  eapply HtBind.
+  eapply HtAtomic.
+  simp.
+  simp.
+  instantiate (1 := (fun n h' => n >= init)).
+  simp.
+  simp.
+  simp.
+  eapply HtAtomic.
+  simp.
+  simp.
+  simp.
+  eapply HtBind.
+  eapply HtAtomic.
+  simp.
+  simp.
+  instantiate (1 := (fun n h' => n >= init)).
+  simp.
+  simp.
+  simp.
+  eapply HtAtomic.
+  simp.
+  simp.
+  simp.
+  simp.
+  simp.
+  simp.
+  simp.
+  simp.
+Qed.
 
 (* Part 3: prove soundness of the program logic *)
 
@@ -97,23 +139,23 @@ Admitted.
 (* Here are a few more lemmas that you may find helpful: *)
 
 Lemma HtStrengthen : forall {t : Set} P R c G (Q : t -> _) (P' : hprop),
-    hoare_triple P R c G Q
-    -> (forall h, P' h -> P h)
-    -> stableP P' R
-    -> hoare_triple P' R c G Q.
+  hoare_triple P R c G Q
+  -> (forall h, P' h -> P h)
+  -> stableP P' R
+  -> hoare_triple P' R c G Q.
 Proof. eauto. Qed.
 
 Lemma HtWeakenFancy : forall {t : Set} P R c G Q (G' : hrel) (Q' : t -> hprop),
-    hoare_triple P R c G Q
-    -> (forall v h, Q v h -> Q' v h)
-    -> (forall h h', G h h' -> G' h h')
-    -> hoare_triple P R c G' Q'.
+  hoare_triple P R c G Q
+  -> (forall v h, Q v h -> Q' v h)
+  -> (forall h h', G h h' -> G' h h')
+  -> hoare_triple P R c G' Q'.
 Proof. eauto using always_stableP. Qed.
 
 Lemma HtReturn' : forall {t : Set} (P : hprop) (R G : hrel) (Q : _ -> hprop) (v : t),
-    stableP P R
-    -> (forall h, P h -> Q v h)
-    -> hoare_triple P R (Return v) G Q.
+  stableP P R
+  -> (forall h, P h -> Q v h)
+  -> hoare_triple P R (Return v) G Q.
 Proof.
   simplify.
   eapply HtConsequence with (P' := P) (R' := R) (G' := G); eauto.
@@ -124,7 +166,7 @@ Lemma stableP_self : forall h R, stableP (fun h' => R^* h h') R.
 Proof. simp. eauto using trc_trans. Qed.
 
 Lemma stableP_star : forall R h h', R^* h h' ->
-    forall P, stableP P R -> P h -> P h'.
+                               forall P, stableP P R -> P h -> P h'.
 Proof. induct 1; simplify; eauto. eapply IHtrc; eauto. Qed.
 
 Lemma stableP_weakenR : forall P R, stableP P R -> forall R' : hrel,
@@ -144,21 +186,271 @@ Proof. unfold stable; propositional; eauto. Qed.
 Local Hint Resolve stable_stableP : core.
 
 Lemma trc_imply :forall t R (s s' : t), R^* s s' ->
-  forall (R':_->_->Prop), (forall s s', R s s' -> R' s s') ->
-  R'^* s s'.
+                                   forall (R':_->_->Prop), (forall s s', R s s' -> R' s s') ->
+                                                 R'^* s s'.
 Proof. induct 1; simplify; eauto. Qed.
 
 Local Hint Extern 1 (_ >= _) => linear_arithmetic : core.
 Local Hint Constructors notAboutToFail : core.
 
-
-(* HINT 1-6 (see Pset12Sig.v) *)   
-Theorem hoare_triple_sound : forall (t : Set) P (c : cmd t) Q,
-  hoare_triple P (fun _ _ => False) c (fun _ _ => True) Q ->
-  forall h, P h ->
-  invariantFor (trsys_of h c) (fun st => notAboutToFail (snd st)).
+Lemma progress :
+  forall (t : Set) P (c : cmd t) R G Q,
+  hoare_triple P R c G Q ->
+  forall h, P h -> notAboutToFail c.
 Proof.
+  induct 1; simplify.
+  eapply NatfBind.
+  eauto.
+  eapply NatfPar.
+  eauto.
+  eauto.
+  equality.
+  eapply NatfReturn.
+  eauto.
+  eauto.
+  eauto.
+Qed.
+
+Lemma guarantee :
+  forall (t : Set) P (c : cmd t) R G Q,
+  hoare_triple P R c G Q -> forall h,
+    P h -> forall h' c', step (h, c) (h', c') -> G^* h h'.
+Proof.
+  induct 1; simplify.
+  invert H3.
+  existT_subst.
+  eauto.
+  existT_subst.
+  eauto.
+  invert H5.
+  existT_subst.
+  eapply trc_imply.
+  eapply IHhoare_triple1.
+  eapply H2.
+  eauto.
+  eauto.
+  eauto.
+  existT_subst.
+  eapply trc_imply.
+  eapply IHhoare_triple2.
+  eapply H3.
+  eauto.
+  eauto.
+  eauto.
+  eauto.
+  invert H1.
+  invert H3.
+  existT_subst.
+  eauto.
+  invert H3.
+  existT_subst.
+  eauto.
+  eapply trc_imply.
+  eapply IHhoare_triple.
+  eauto.
+  eauto.
+  eauto.
+Qed.
+
+Lemma preservation :
+  forall (t : Set) P (c : cmd t) R G Q,
+  hoare_triple P R c G Q ->
+  forall h, P h -> forall h' c', step (h, c) (h', c') ->
+                      hoare_triple (fun h'' => R^* h' h'') R c' G Q.
+Proof.
+  induct 1; simp.
+
+  (*bind case*)
+  invert H3.
+  simp.
+  eapply HtBind.
+  eapply IHhoare_triple.
+  eauto.
+  eauto.
+  apply H0.
+  simp.
+  eapply HtStrengthen.
+  apply H0.
+  simp.
+  (* had a tough time figuring out this case in return *)
+  admit.
+  eauto.
+
+  (*par case*)
+  invert H5.
+  simp.
+  eapply HtPar.
+  eapply IHhoare_triple1.
+  eauto.
+  eauto.
+  eapply H0.
+  eauto.
+  simp.
+  eapply trc_imply.
+  eauto.
+  eauto.
+  simp.
+  assert ( (G1) ^* h h').
+  eapply guarantee.
+  apply H.
+  eauto.
+  eauto.
+  apply always_stableP in H0.
+  eapply stableP_star.
+  apply H5.
+  eapply stableP_weakenR.
+  apply H0.
+  simp.
+  eapply stableP_star.
+  apply H6.
+  eapply stableP_weakenR.
+  eauto.
+  simp.
+  eauto.
+  simp.
+  eapply HtPar.
+  apply H.
+  eapply IHhoare_triple2.
+  eauto.
+  eauto.
+  eauto.
+  assert ( (G2) ^* h h').
+  eapply guarantee.
+  apply H0.
+  eauto.
+  eauto.
+  simp.
+  apply always_stableP in H.
+  eapply stableP_star.
+  apply H6.
+  eapply stableP_weakenR.
+  apply H.
+  simp.
+  eapply stableP_star.
+  apply H5.
+  eapply stableP_weakenR.
+  apply H.
+  simp.
+  eauto.
+  simp.
+  eapply trc_imply.
+  eauto.
+  eauto.
+
+  (*return case -- atomics*)
+  invert H1.
+  invert H3.
+  simp.
+  eapply HtWeakenFancy.
+  eapply HtReturn.
+  eauto.
+  simp.
+  eapply stable_stableQ in H1.
+  eapply stableP_star.
+  apply H4.
+  apply H1.
+  eapply H0.
+  eauto.
+  eauto.
+  eauto.
+  eapply HtWeakenFancy.
+  eapply HtReturn.
+  eauto.
+  simp.
+  eapply stable_stableQ in H1.
+  eapply stableP_star.
+  apply H4.
+  apply H1.
+  eapply H0.
+  eauto.
+  eauto.
+  eauto.
+
+  (*loop case*)
+  invert H3.
+  simp.
+  step.
+  eapply HtStrengthen.
+  eapply H.
+  simp.
+  eapply stableP_star.
+  eauto.
+  eapply always_stableP.
+  eapply H.
+  eauto.
+  eauto.
+  cases r; simp.
+  eapply HtWeakenFancy.
+  eapply HtReturn.
+  eapply H1.
+  simp.
+  eauto.
+  eauto.
+
+
+  eapply HtConsequence.
+  eapply IHhoare_triple.
+  eauto.
+  eauto.
+  simp.
+  eapply trc_imply.
+  eapply H7.
+  eauto.
+  eauto.
+  eauto.
+  eauto.
+  eauto.
 Admitted.
+
+(* HINT 1-6 (see Pset12Sig.v) *)
+Theorem hoare_triple_sound :
+  forall (t : Set) P (c : cmd t) Q,
+  hoare_triple P
+    (fun _ _ => False)
+    c
+    (fun _ _ => True)
+    Q -> forall h, P h -> invariantFor (trsys_of h c)
+                     (fun st => notAboutToFail (snd st)).
+Proof.
+  simp.
+  apply invariant_weaken with
+    (fun st => exists P, hoare_triple
+              P
+              (fun _ _ : heap => False)
+              (snd st)
+              (fun _ _ : heap => True)
+              Q /\ P (fst st)).
+
+  2: {
+    simp.
+    eapply progress.
+    eauto.
+    eauto.
+  }
+
+  eapply invariant_induction.
+  simp.
+  eauto.
+  simp.
+  cases s; simp.
+  cases s'; simp.
+  assert (hoare_triple
+            (fun h : heap => (fun _ _ : heap => False) ^* f0 h)
+            (fun _ _ : heap => False)
+            c1
+            (fun _ _ : heap => True)
+            Q).
+  eapply HtStrengthen.
+  eapply preservation.
+  apply H1.
+  apply H4.
+  apply H2.
+  simp.
+  simp.
+  assert ((fun h : heap => (fun _ _ : heap => False) ^* f0 h) f0).
+  eauto.
+  eauto.
+Qed.
 End Impl.
 
 Module ImplCorrect : Pset12Sig.S := Impl.
